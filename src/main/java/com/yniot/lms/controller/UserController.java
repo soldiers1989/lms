ac24b2d0086cddbc3bd4681652861c0f66fd9b69
lane <wglbsr@163.com>
2018-11-14 16:59:23 +0800

    update

--- src/main/java/com/yniot/lms/controller/UserController.java
+++ src/main/java/com/yniot/lms/controller/UserController.java
@@ -1,4 +1,14 @@
 package com.yniot.lms.controller;
 
-public class UserController {
+import com.yniot.lms.controller.commonController.BaseController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController extends BaseController {
+    @RequestMapping(name="/test")
+    public String test(){
+        return super.getSuccessResult("hello");
+    }
+
 }

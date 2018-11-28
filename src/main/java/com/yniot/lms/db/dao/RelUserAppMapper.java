de0241061eeb1397b0591064586d909e6e8e9b5e
lane <wglbsr@163.com>
2018-11-28 17:03:57 +0800

    update

---
+++ src/main/java/com/yniot/lms/db/dao/RelUserAppMapper.java
@@ -0,0 +1,10 @@
+package com.yniot.lms.db.dao;
+
+import com.yniot.exclude.CommonMapper;
+import com.yniot.lms.db.entity.RelUserApp;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface RelUserAppMapper extends CommonMapper<RelUserApp> {
+
+}
---
+++ src/main/java/com/yniot/lms/service/impl/RelUserAppServiceImpl.java
@@ -0,0 +1,17 @@
+package com.yniot.lms.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.yniot.lms.db.dao.RelUserAppMapper;
+import com.yniot.lms.db.entity.RelUserApp;
+import com.yniot.lms.service.RelUserAppService;
+import org.springframework.stereotype.Service;
+
+/**
+ * @project: lms
+ * @description:
+ * @author: wanggl
+ * @create: 2018-11-22 19:13
+ **/
+@Service
+public class RelUserAppServiceImpl extends ServiceImpl<RelUserAppMapper, RelUserApp> implements RelUserAppService {
+}
